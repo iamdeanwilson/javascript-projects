a9132eb6c3b59925f3d4b0a6558da8d9bb27ae9e
Dean Wilson <Dean Wilson>
2023-09-09 19:30:01 -0500

    Added space exploration mission

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,35 @@
 // Declare and assign the variables below
 
+let nameOfTheSpaceShuttle = "Determination";
+let shuttleSpeedMPH = 17500;
+let distanceToMarsKM = 225000000;
+let distanceToTheMoonKM = 384400;
+let milesPerKilometer = 0.621;
+
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+
+console.log(typeof nameOfTheSpaceShuttle)
+console.log(typeof shuttleSpeedMPH)
+console.log(typeof distanceToMarsKM)
+console.log(typeof distanceToTheMoonKM)
+console.log(typeof milesPerKilometer)
 
 // Calculate a space mission below
 
+let milesToMars = (distanceToMarsKM * milesPerKilometer)
+let hoursToMars = (milesToMars / shuttleSpeedMPH)
+let daysToMars = (hoursToMars / 24)
+
 // Print the results of the space mission calculations below
+
+console.log(nameOfTheSpaceShuttle + " will take " + daysToMars + " days to reach Mars.")
 
 // Calculate a trip to the moon below
 
+let milesToMoon = (distanceToTheMoonKM * milesPerKilometer)
+let hoursToMoon = (milesToMoon / shuttleSpeedMPH)
+let daysToMoon = (hoursToMoon / 24)
+
 // Print the results of the trip to the moon below
+
+console.log(nameOfTheSpaceShuttle + " will take " + daysToMoon + " days to reach the Moon.")
