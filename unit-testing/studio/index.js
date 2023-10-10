26b12dc83b47f76a63f97a5933335a5775292c4b
Dean Wilson <Dean Wilson>
2023-10-09 19:27:09 -0500

    completed Unit Testing Studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,52 @@
-
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function launchOutput(num){
+        let output = ''
+        let divisibleNums = [2,3,5];
+        let responses = ['Launch', 'Code ', 'Rocks'];
+        for (let i = 0 ; i < divisibleNums.length ; i++){
+            if  (num % divisibleNums[i] === 0){
+                output += responses[i];
+            }
+        }
+        output = (output.trim() + '!');           
+        if (output === '!'){
+            output = "Rutabagas! That doesn't work.";
+        }
+        if (output === 'LaunchRocks!'){
+            output = "Launch Rocks! (CRASH!!!!)";
+        }        
+        return output
+        
+        // OLD CODE, BEFORE REFACTORING
+        
+        // if  (num % 2 === 0 && num % 3 === 0 && num % 5 === 0){
+        //     return 'LaunchCode Rocks!';
+        // }  
+        // if  (num % 2 === 0 && num % 3 === 0){
+        //     return 'LaunchCode!';
+        // }  
+        // if  (num % 3 === 0 && num % 5 === 0){
+        //     return 'Code Rocks!';
+        // } 
+        // if  (num % 2 === 0 && num % 5 === 0){
+        //     return 'Launch Rocks! (CRASH!!!!)';
+        // }         
+        // if  (num % 2 === 0){
+        //     return 'Launch!';
+        // }
+        // if  (num % 3 === 0){
+        //     return 'Code!';
+        // }
+        // if  (num % 5 === 0){
+        //     return 'Rocks!';
+        // }
+
+        //else return "Rutabagas! That doesn't work."
+    }
 }
 
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,48 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test("should return 'Launch!' when the number provided is ONLY divisible by 2", function() {
+    let output = launchcode.launchOutput(2);
+    expect(output).toEqual("Launch!");
+  });
+
+  test("should return 'Code!' when the number provided is ONLY divisible by 3", function() {
+    let output = launchcode.launchOutput(3);
+    expect(output).toEqual("Code!");
+    });
+
+  test("should return 'Rocks!' when the number provided is ONLY divisible by 5", function() {
+    let output = launchcode.launchOutput(5);
+    expect(output).toEqual("Rocks!");
+    });
+
+  test("should return 'LaunchCode!' when number provided is divisible by both 2 AND 3", function() {
+    let output = launchcode.launchOutput(6);
+    expect(output).toEqual("LaunchCode!");
+    });
+
+  test("should return 'Code Rocks!' when number provided is divisible by both 3 AND 5", function() {
+    let output = launchcode.launchOutput(15);
+    expect(output).toEqual("Code Rocks!");
+    });
+
+  test("should return 'Launch Rocks! (CRASH!!!!)' when number provided is divisible by both 2 AND 5", function() {
+    let output = launchcode.launchOutput(10);
+    expect(output).toEqual("Launch Rocks! (CRASH!!!!)");
+    });
+
+  test("should return 'LaunchCode Rocks!' when number provided is divisible by 2, 3, AND 5", function() {
+    let output = launchcode.launchOutput(30);
+    expect(output).toEqual("LaunchCode Rocks!");
+    });
+
+  test("should return 'Rutabagas! That doesn't work.' when number provided is NOT divisible by 2, 3, or 5", function() {
+    let output = launchcode.launchOutput(7);
+    expect(output).toEqual("Rutabagas! That doesn't work.");
+    });
+    
+  // test("should return ", function() {
+  //   let output = launchcode.launchOutput();
+  //   expect(output).toEqual("");
+  //   });      
 });
