f732695fe2542fcffb5672e87029b8a43dfe3784
Dean Wilson <Dean Wilson>
2023-10-29 21:39:10 -0500

    Completed DOM and Events Exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    liftoffButton.addEventListener('click', event => {
+        statusReport.innerHTML = 'Houston! We have liftoff!';
+     });
+
+    abortMission.addEventListener('mouseover', event => {
+        event.target.style.background = "red";
+    });
+    abortMission.addEventListener('mouseout', event => {
+        event.target.style.background = "";
+    });            
+
+    abortMission.addEventListener('click', function {
+        let answer = window.confirm("Are you sure you want to abort the mission?");
+        if (answer === true){
+            statusReport.innerHTML = 'Mission aborted! Space shuttle returning home';
+        }
+    });
+
     
 }
 
