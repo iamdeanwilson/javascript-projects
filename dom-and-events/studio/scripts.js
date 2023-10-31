792ca165ef3d2cb0561dfe78a2d3e7115ff985cd
Dean Wilson <Dean Wilson>
2023-10-30 20:27:47 -0500

    studio

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,57 @@
 // Write your JavaScript code here.
+window.addEventListener("load", function() {
+
+takeoff.addEventListener('click', event => {
+    let answer = window.confirm('Confirm that the shuttle is ready for takeoff.');
+    if (answer === true){
+        flightStatus.innerHTML = 'Shuttle in flight.';
+        spaceShuttleHeight.innerHTML = Number(Number(spaceShuttleHeight.innerHTML)+10000);
+        shuttleBackground.style.backgroundColor = 'blue'
+    }
+ });
+
+ landing.addEventListener('click', event => {
+        window.alert('The shuttle is landing. Landing gear engaged.');
+        flightStatus.innerHTML = 'The shuttle has landed.';
+        spaceShuttleHeight.innerHTML = 0;
+        shuttleBackground.style.backgroundColor = ''
+ });
+
+ missionAbort.addEventListener('click', event => {
+    let answer = window.confirm('Confirm that you want to abort the mission.');
+    if (answer === true){
+        flightStatus.innerHTML = 'Mission aborted.';
+        spaceShuttleHeight.innerHTML = 0;
+        shuttleBackground.style.backgroundColor = ''
+    }
+ });
+ 
+ up.addEventListener('click', event => {
+    rocket.style.position.top =  (rocket.style.position.top + parseInt('10px'));
+    spaceShuttleHeight.innerHTML = Number(Number(spaceShuttleHeight.innerHTML)+10000);
+ });
+
+ down.addEventListener('click', event => {
+    rocket.style.position.top =  (rocket.style.position.top - parseInt('10px'));
+    spaceShuttleHeight.innerHTML = Number(Number(spaceShuttleHeight.innerHTML)-10000);
+ });
+
+ right.addEventListener('click', event => {
+    rocket.style.position.right =  (rocket.style.position.right - parseInt('10px'));
+ });
+
+ left.addEventListener('click', event => {
+    rocket.style.position.left =  (rocket.style.position.left - parseInt('10px'));
+ });
+
+});
+
+
+
+
+
+
+
+
+
 // Remember to pay attention to page loading!
